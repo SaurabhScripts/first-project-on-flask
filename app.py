421a069b01b40f3267afee3f60e9773cf75fd35b
saurabhmkgp <127095776+Saurabhsterlite@users.noreply.github.com>
2023-04-06 10:22:30 +0000

    change

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask 
+from flask import Flask ,render_template
 
 app =Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "hello, User"
+  return render_template('home.html')
 if __name__ =="__main__":
-  print("im inside the if now")
+  
+
   app.run(host='0.0.0.0',debug=True)
